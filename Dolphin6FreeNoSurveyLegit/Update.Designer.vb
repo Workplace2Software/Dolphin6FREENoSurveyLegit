b0a413d7d63af6cf53947e78605afbac2345903a
soafen <soafensoe@protonmail.com>
2022-05-06 16:13:42 -0700

    v1.0.2 label

--- Dolphin6FreeNoSurveyLegit/Update.Designer.vb
+++ Dolphin6FreeNoSurveyLegit/Update.Designer.vb
@@ -25,6 +25,7 @@ Partial Class UpdateD
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
@@ -56,18 +57,30 @@ Partial Class UpdateD
         Me.Button1.Text = "Awsome"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'Update
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.DimGray
+        Me.Label3.Location = New System.Drawing.Point(12, 64)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 24)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "v1.0.2"
+        '
+        'UpdateD
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(382, 97)
         Me.ControlBox = False
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
-        Me.Name = "Update"
+        Me.Name = "UpdateD"
         Me.Text = "Update"
         Me.ResumeLayout(False)
         Me.PerformLayout()
@@ -76,4 +89,5 @@ Partial Class UpdateD
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
 End Class
